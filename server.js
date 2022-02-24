45c8e2b0626121b0a6d111bd61c7bae4aeeb83ea
Michał Szymański <michal.szymanski1908@gmail.com>
2022-02-24 14:34:03 +0100

    Final 29.5

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
-const socket = require('socket.io');
+const mongoose = require('mongoose');
+
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
+app.use;
+
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
@@ -31,16 +30,17 @@ app.use((req, res) => {
 });
 
 // server
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running');
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const db = mongoose.connection;
 
-const io = socket(server);
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error' + err));
 
-io.on('connection', (socket) => {
-  console.log('a user connected ' + socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Socket ' + socket.id + ' left');
-  });
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running');
 });
